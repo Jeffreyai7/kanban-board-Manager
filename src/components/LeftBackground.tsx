d34f112fb407167747aec5b175b7920ecdc29075
jeffreyai7 <jirukeh@gmail.com>
2025-04-13 19:02:13 +0100

    initialize structure of login page

--- src/components/LeftBackground.tsx
+++ src/components/LeftBackground.tsx
@@ -1,13 +1,13 @@
 const LeftBackground = () => {
   return (
-    <div
-      className="hidden md:flex w-1/2 min-w-[480px] bg-cover bg-center bg-[#5c40fd] text-white"
-      style={{ backgroundImage: "url('images/serverbackground.png')" }}
-    >
+    <div className="hidden md:flex w-1/2 min-w-[480px] bg-cover bg-center bg-[#5c40fd] text-white">
       <div className="w-[480px] bg-opacity-50 md:pt-[calc(120px-4%)] md:pl-[calc(170px-4%)]  lg:pl-[calc(120px-3%)] lg:pt-[calc(125px-3%)] xl:pt-[125px] xl:pl-[159px]">
         <div className="">
           <div className="flex items-center gap-1 md:mb-[calc(266px-70%)] lg:mb-[calc(240px-50%)] xl:mb-[240px]">
-            <img src="/icons/logo.svg" width={40} height={40} alt="logo" />
+            <div className="flex gap-0.5 border border-gray-200 rounded p-1">
+              <div className="h-6 w-2 bg-blue-700 rounded"></div>
+              <div className="h-4 w-2 bg-blue-700 rounded"></div>
+            </div>
             <p
               className="leading-[49px] font-semibold text-[3rem]"
               style={{ fontFamily: "SF Pro Rounded, sans-serif" }}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,66 @@
+import LeftBackground from "../components/LeftBackground";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="flex flex-col w-full min-h-screen">
+      <div className="flex flex-col md:flex-row w-full h-screen shadow-lg  overflow-hidden">
+        <LeftBackground />
+        <div className="w-full md:w-1/2 min-w-[480px] relative bg-white h-screen overflow-y-auto">
+          <div className="max-w-md mx-auto lg:mt-24 xl:mt-52">
+            <h1 className="text-[1.8rem] text-center leading-[36px] font-bold">
+              Log in to your account
+            </h1>
+            <p className="text-[1rem] text-[#6B7280] text-center leading-[24px]">
+              Access your dashboard to manage your projects and tasks.
+            </p>
+            <form
+              action="
+            "
+            >
+              <div className="mt-8">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+
+              <div className="mt-6">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full mt-6 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md transition duration-200 ease-in-out"
+              >
+                Log in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
