722c2017450dba6e6847a0898e83e83e6fd88446
PrinceTheDev <sinbadprince9@gmail.com>
2025-04-15 23:09:06 +0100

    Secured passwords in .env file

--- kanban_backend/users/apps.py
+++ kanban_backend/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class KanbanApiConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kanban_api'
+    name = 'users'
