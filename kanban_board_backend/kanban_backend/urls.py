12921cec903905260e6b5d5f768ea6f26afdc5ef
PrinceTheDev <sinbadprince9@gmail.com>
2025-05-18 17:17:10 +0100

    commit changes

--- kanban_board_backend/kanban_backend/urls.py
+++ kanban_board_backend/kanban_backend/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),
+    path('api/tasks', include('tasks.urls')),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/users/', include('users.urls')),
--- kanban_board_backend/users/urls.py
+++ kanban_board_backend/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import UserDetailView, SendCodeView, VerifyCodeView
-from .views import GoogleLogin
+from .views import UserDetailView, SendCodeView, VerifyCodeView, GoogleLogin
 
 
 urlpatterns = [
     path('', UserDetailView.as_view(), name='user-detail'),
-    path('send-code/', SendCodeView.as_view(), name='send-code'),
-    path('verify-code/', VerifyCodeView.as_view(), name='verify-code'),
-    path('google/', GoogleLogin.as_view(), name='google_login'),
+    path('users/send-code/', SendCodeView.as_view(), name='send-code'),
+    path('users/verify-code/', VerifyCodeView.as_view(), name='verify-code'),
+    path('users/google/', GoogleLogin.as_view(), name='google_login'),
 ]
